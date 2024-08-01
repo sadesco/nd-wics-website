1a9f3bf89510c35c63b7d1b5755597538fd15509
sadesco <147342700+sadesco@users.noreply.github.com>
2024-07-31 21:05:16 -0400

    fixing the loading problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/*
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
@@ -15,6 +16,35 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} exact />
+          <Route path="/About" element={<About />} />
+          <Route path="/Team" element={<Team />} />
+          <Route path="/Events" element={<Events />} />
+          <Route path="/GetInvolved" element={<GetInvolved />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+*/
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Team from './Team';
+import Events from './Events';
+import GetInvolved from './GetInvolved';
+
+function App() {
+  console.log('App component rendered');
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Team" element={<Team />} />
           <Route path="/Events" element={<Events />} />
