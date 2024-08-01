c6effe4ca7d28aa95c655a55f2a12a58dad498b6
sadesco <147342700+sadesco@users.noreply.github.com>
2024-07-31 23:27:17 -0400

    rerouting problem

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 }
 
 export default App;
-*/
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
@@ -41,6 +41,35 @@ function App() {
   console.log('App component rendered');
   return (
     <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Team" element={<Team />} />
+          <Route path="/Events" element={<Events />} />
+          <Route path="/GetInvolved" element={<GetInvolved />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+*/
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Team from './Team';
+import Events from './Events';
+import GetInvolved from './GetInvolved';
+
+function App() {
+  console.log('App component rendered');
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Header />
         <Routes>
